50c7412cb68625815325c075ebd6c5540e1503dd
Ram Munde <77351570+ram123-debug@users.noreply.github.com>
2022-07-15 00:25:48 +0530

    simple recursive program for DFS

---
+++ Graph: Depth-First Search/Program.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Program {
+  // Do not edit the class below except
+  // for the depthFirstSearch method.
+  // Feel free to add new properties
+  // and methods to the class.
+  static class Node {
+    String name;
+    List<Node> children = new ArrayList<Node>();
+
+    public Node(String name) {
+      this.name = name;
+    }
+
+    public List<String> depthFirstSearch(List<String> array) {
+      // Write your code here.
+      array.add(name);
+      for(Node child:children)
+      {
+       child.depthFirstSearch(array);
+      }      return array;
+    }
+
+    public Node addChild(String name) {
+      Node child = new Node(name);
+      children.add(child);
+      return this;
+    }
+  }
+}
