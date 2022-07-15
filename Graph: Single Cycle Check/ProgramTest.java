4d5d23e2f3feaeb6ad68935807f794ea1dba6a1b
Ram Munde <rammunde19@gmail.com>
2022-07-15 10:27:17 +0530

    Create ProgramTest.java

---
+++ Graph: Single Cycle Check/ProgramTest.java
@@ -0,0 +1,10 @@
+// This file is initialized with a code version of this
+// question's sample test case. Feel free to add, edit,
+// or remove test cases in this file as you see fit!
+
+class ProgramTest {
+  @Test
+  public void TestCase1() {
+    Utils.assertTrue(Program.hasSingleCycle(new int[] {2, 3, 1, -4, -4, 2}));
+  }
+}
