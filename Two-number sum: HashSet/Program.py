677fb438ef913274aae988f32de6bdf37a5e7e1b
Ram Munde <rammunde19@gmail.com>
2022-07-16 08:45:53 +0530

    O(n^2) time complexity using two loops

---
+++ Two-number sum: HashSet/Program.py
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Program {
+  public static int[] twoNumberSum(int[] array, int targetSum) {
+    // Write your code here.
+    int[] arr=new int [2];
+    for(int i=0;i<array.length;i++){
+       for(int j=i;j<array.length;j++){
+         if((array[i] + array [j]) == targetSum && array[i]!=array[j]){ 
+           arr[0]=array[i];
+           arr[1]=array[j];
+           return arr;
+           }
+        
+         }
+       }
+      return new int[0];
+      }
+  
+}
