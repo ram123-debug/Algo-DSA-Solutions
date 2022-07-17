7805fdfb8dbcd0891a0b3cc6e7ad7a9f00142dcd
Ram Munde <rammunde19@gmail.com>
2022-07-17 09:27:38 +0530

    Create Program_iterative.java

---
+++ Node-Depths: Binary Tree/Program_iterative.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Program {
+
+  public static int nodeDepths(BinaryTree root) {
+    // Write your code here.
+    int sumOfDepths = 0;
+    List<Level> stack = new ArrayList<Level> ();
+    stack.add(new Level(root, 0));
+    while(stack.size() > 0){
+      Level top = stack.remove(stack.size()-1);
+      BinaryTree node = top.root;
+      int depth = top.depth;
+      if(node == null) continue;
+      sumOfDepths += depth;
+      stack.add(new Level (node.left, depth+1));
+      stack.add(new Level(node.right, depth+1));
+    }
+    return sumOfDepths;
+  }
+  static class Level {
+    public BinaryTree root;
+    int depth;
+    public Level(BinaryTree root, int depth){
+      this.root = root;
+      this.depth =depth;
+    }
+  }
+  static class BinaryTree {
+    int value;
+    BinaryTree left;
+    BinaryTree right;
+
+    public BinaryTree(int value) {
+      this.value = value;
+      left = null;
+      right = null;
+    }
+  }
+}
