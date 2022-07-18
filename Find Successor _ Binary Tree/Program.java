c5c98aebe6575cae7e13f642810811f8b5c075d5
Ram Munde <rammunde19@gmail.com>
2022-07-18 10:07:56 +0530

    O(h) time | O(1) space - where h is the height of the tree

---
+++ Find Successor _ Binary Tree/Program.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Program {
+  // This is an input class. Do not edit.
+  static class BinaryTree {
+    public int value;
+    public BinaryTree left = null;
+    public BinaryTree right = null;
+    public BinaryTree parent = null;
+
+    public BinaryTree(int value) {
+      this.value = value;
+    }
+  }
+ // O(h) time | O(1) space - where h is the height of the tree
+  public BinaryTree findSuccessor(BinaryTree tree, BinaryTree node) {
+    // Write your code here.
+    if(node.right != null ) return getLeftmostChild(node.right);
+    return getRightmostParent(node);
+  }
+
+  public BinaryTree getLeftmostChild (BinaryTree node){
+    BinaryTree currentNode = node;
+    while(currentNode.left != null){
+      currentNode = currentNode.left;
+    }
+    return currentNode;
+  }
+  public BinaryTree getRightmostParent(BinaryTree node){
+    BinaryTree currentNode = node;
+    while(currentNode.parent != null && currentNode.parent.right == currentNode){
+      currentNode = currentNode.parent;
+    }
+    return currentNode.parent;
+  }
+}
