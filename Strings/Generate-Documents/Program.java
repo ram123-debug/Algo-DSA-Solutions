2e8abcd2a65289a8b1a4b8417553d12e400d58f5
Ram Munde <rammunde19@gmail.com>
2022-07-18 20:06:39 +0530

    O(m+n) time where m and n is the size of documents and characters respectively

---
+++ Strings/Generate-Documents/Program.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Program {
+
+  public boolean generateDocument(String characters, String document) {
+    // Write your code here.
+    HashMap<Character, Integer> characterCounts = new HashMap<Character, Integer>();
+    for(int idx =0 ; idx < characters.length();idx++){
+      char character = characters.charAt(idx);
+      characterCounts.put(character, characterCounts.getOrDefault(character, 0)+1);
+    }
+    for(int idx=0; idx < document.length();idx++)
+      {
+        char character = document.charAt(idx);
+        if(!characterCounts.containsKey(character) || characterCounts.get(character)==0)
+        {
+          return false;
+        }
+        characterCounts.put(character, characterCounts.get(character)- 1);
+      }
+    return true;
+  }
+}
