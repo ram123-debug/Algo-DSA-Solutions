6afae2180991781664849c7bcb15106597b2f57b
Ram Munde <rammunde19@gmail.com>
2022-07-20 20:10:45 +0530

    O(n) time and O(n) space

---
+++ Balanced-Brackets/Program.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Program {
+  public static boolean balancedBrackets(String str) {
+    // Write your code here.
+    String openingBrackets = "([{";
+    String closingBrackets = ")]}";
+    Map<Character, Character> matchingBrackets = new HashMap<Character, Character>();
+    matchingBrackets.put(')','(');
+    matchingBrackets.put(']', '[');
+    matchingBrackets.put('}','{');
+    List<Character> stack = new ArrayList<Character>();
+
+    for(int i=0;i < str.length();i++)
+      {
+        char letter = str.charAt(i);
+        if(openingBrackets.indexOf(letter) != -1)
+        {
+          stack.add(letter);
+        } else if (closingBrackets.indexOf(letter) != -1){
+          if(stack.size()==0) return false;
+          if(stack.get(stack.size()-1)== matchingBrackets.get(letter))
+          {
+            stack.remove(stack.size() - 1);
+            
+          }
+         else return false;}
+      }
+    return stack.size() == 0;
+  }
+}
