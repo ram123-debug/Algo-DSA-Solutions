f6fef75cf5b1947edc4837945e306226d312f7f4
Ram Munde <rammunde19@gmail.com>
2022-07-22 11:20:13 +0530

    O(n) Time | O(n) Space

---
+++ Next-Greater Stack/Program.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Program {
+
+  public int[] nextGreaterElement(int[] array) {
+    // Write your code here.
+    int [] result = new int[array.length];
+    Arrays.fill(result, -1);
+
+    Stack<Integer> stack = new Stack<Integer> ();
+
+    for(int idx = 0; idx < 2* array.length ; idx++)
+      {
+        int circularIdx = idx%array.length;
+        while(stack.size() > 0 && array[stack.peek()] < array[circularIdx])
+          {
+            int top = stack.pop();
+            result[top] = array[circularIdx];
+          }
+        stack.push(circularIdx);
+      }
+    return result;
+  }
+}
